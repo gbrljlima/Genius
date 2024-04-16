498c46f5d9c1747f68ce90283e8a621942307793
Gabriel Lima <124406585+gbrljlima@users.noreply.github.com>
2024-04-16 13:12:46 -0300

    Att bloqueio e opacidade do botão iniciar

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
     const cores = ["verde", "amarelo", "azul", "vermelho"];
     let x = Math.floor((Math.random() * cores.length));
-    var corRandom = [];
+    let corRandom = [];
     corRandom.unshift(cores.at(x));
     
     let botao;
-    var corEscolhida = [];
-    var contador = 1;
-    var contadorTentativa = 0;
+    let botoes = document.getElementsByClassName("botao");
+    let corEscolhida = [];
+    let contador = 1;
+    let contadorTentativa = 0;
     let pontuacao = 0;
     let record = 0;
 
@@ -38,17 +39,25 @@
             document.getElementById("pontuacao").innerHTML = "Pontuação: " + pontuacao;         
         }
         else {
+            for (let i = 0; i < botoes.length; i++) {
+                botoes[i].disabled = true;
+                botoes[i].style.cursor = "not-allowed";
+            }
             if (pontuacao > record) {
                 record = pontuacao;
                 document.getElementById("melhorPontuacao").innerHTML = "Melhor pontuação: " + record;
             }
-            document.getElementById("iniciar").style.display = "flex";
-            corEscolhida = [];
+            
             x = Math.floor((Math.random() * cores.length));
             corRandom.splice(0, corRandom.length, cores.at(x));
+            
+            corEscolhida = [];
             contador = 1;
             contadorTentativa = 0;
             pontuacao = 0;
+            
+            document.getElementById("iniciar").style.display = "flex";
+            
         }
     }
     
@@ -77,7 +86,6 @@
         setTimeout(function() {
             botao.classList.remove("active");
         }, 500);
-        let botoes = document.getElementsByClassName("botao");
         for (let i = 0; i < botoes.length; i++) {
             botoes[i].disabled = false;
             botoes[i].style.cursor = "pointer";
